f57eee88781449e2fa0ab1c372fa256ca6a8d9e0
Breno430 <brenodias002@gmail.com>
2020-08-24 16:13:08 -0300

    primeira_single_applicantion

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,27 +2,50 @@
   <div class="corpo">
     <h1 class="centralizado" v-text="titulo"></h1>
 
+    <input type="search" class="filtro"  @input="filtro= $event.target.value" placeholder="filtre pelo título da foto">
+    {{ filtro }}
+
     <ul class="listar-fotos">
-      <li class="listar-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">        
-        <img :src="foto.url" :alt="foto.titulo" />
-        </div>
-        </div>
+      <li class="listar-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo" >
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+        </meu-painel>
       </li>
     </ul>
   </div>
-</template> 
+</template>
 
 <script>
+import Painel from './components/shared/painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva';
+
 export default {
+
+  components: {
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
+  },
+
   data() {
     return {
       titulo: "Breno teste",
       fotos: [],
+      filtro: "",
+      visivel: true
     };
   },
+
+  computed: {
+    fotosComFiltro(){
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
   created() {
     let promisse = this.$http.get("http://localhost:3000/v1/fotos");
     promisse.then((res) => res.json()).then((fotos) => (this.fotos = fotos));
@@ -47,5 +70,10 @@ export default {
 .listar-fotos,
 .listar-fotos-item {
   display: inline-block;
+}
+
+.filtro{
+  display: block;
+  width: 100%;
 }
 </style>
