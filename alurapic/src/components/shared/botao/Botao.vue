20570abf075f49849a5f6a387a6b5d570d7bdede
Breno430 <brenodias002@gmail.com>
2020-08-25 14:24:17 -0300

    vue_js

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -1,11 +1,42 @@
 <template>
-  <button class="botao botao-perigo" :type="tipo">{{rotulo}}</button>
+  <button @click="disparaAcao()" :class="estiloDoBotao" :type="tipo">{{rotulo}}</button>
 </template>
 <script>
 export default {
 
-  props: ['tipo', 'rotulo']
-}
+  props: {
+    tipo: {
+      type: String,
+      required: true
+    },
+
+    rotulo: {
+      type: String,
+      required: true
+    },
+
+    confirmacao: Boolean,
+    estilo: String
+  },
+
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm('Confimar operação??')) {
+          this.$emit('botaoAtivado');
+        }
+        return;
+      }
+      this.$emit('botaoAtivado');
+    }
+    },
+      computed: {
+      estiloDoBotao(){
+        if(this.estilo == 'padrao' || !this.estilo) return 'botao botao-padrao';
+        if(this.estilo == 'perigo') return 'botao botao-perigo';
+      }
+      }
+  }
 </script>
 
 <style scoped>
